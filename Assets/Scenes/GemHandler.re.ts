e6c796e7e1c5fe88edf837e694a59e8ba6552d27
Aria Belcourt <aria@devend.com>
2024-11-14 17:47:35 -0500

    adding a color dropdown select value

--- Assets/Scenes/GemHandler.re.ts
+++ Assets/Scenes/GemHandler.re.ts
@@ -1,21 +1,24 @@
+import RogueCSS2D from '@RE/RogueEngine/rogue-css2d/RogueCSS2D.re';
 import * as RE from 'rogue-engine';
 import * as THREE from 'three';
 
 @RE.registerComponent
 export default class GemHandler extends RE.Component {
-  
+  @RE.props.select() public color: number;
+  colorOptions = ["Red", "Blue", "Green", "Yellow", "Purple"];
   @RE.props.num() public row: number;
   @RE.props.num() public column: number;
 
-  start() {
+  public css2dObject: RogueCSS2D;
 
+  start() {
+    this.css2dObject = RE.getComponent(RogueCSS2D, this.object3d);
   }
 
   update() {
-
+    const colorStr = this.colorOptions[this.color];
+    this.css2dObject.content = `${colorStr}<br>${this.row}, ${this.column}`;
   }
 
-  public setColor(color: THREE.Color) {
-    
-  }
+  
 }
