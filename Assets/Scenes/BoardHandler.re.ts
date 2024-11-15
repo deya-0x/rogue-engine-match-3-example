6cd7cbedd4acecb74bd2b1eb463c6bca5295ebb3
Aria Belcourt <aria@devend.com>
2024-11-14 20:34:41 -0500

    working on the match logic so it can scale

--- Assets/Scenes/BoardHandler.re.ts
+++ Assets/Scenes/BoardHandler.re.ts
@@ -29,29 +29,39 @@ export default class BoardHandler extends RE.Component {
     return this._rows * this._rowHeight;
   }
 
+  private _matchPause: boolean = false;
+
   start() {
     this.createBoard();
   }
 
   update() {
-    
+    if (!this.isAnimating && !this._matchPause) {
+      this.checkForMatches();
+      this._matchPause = false;
+    }
   }
 
   private createBoard() {
     for (let i = 0; i < this._columns; i++) {
       for (let j = 0; j < this._rows; j++) {
-        // Pick a random gem prefab from the list
-        const gemPrefab = this.gems[Math.floor(Math.random() * this.gems.length)];
-        const gem = gemPrefab.instantiate(this.object3d);
-        const gemHandler = RE.getComponent(GemHandler, gem) as GemHandler;
-        gemHandler.row = j;
-        gemHandler.column = i;
-        gem.position.set(i * this._columnWidth, j * this._rowHeight, 0);
+        this.createRandomGemAt(i, j);
       }
     }
 
     // Move the board to the center of the screen
     this.object3d.position.set(-this.boardWidth / 2, 0, 0);
+  }
+
+  private createRandomGemAt(column: number, row: number) {
+    // Pick a random gem prefab from the list
+    const gemPrefab = this.gems[Math.floor(Math.random() * this.gems.length)];
+    const gem = gemPrefab.instantiate(this.object3d);
+    const gemHandler = RE.getComponent(GemHandler, gem) as GemHandler;
+    gemHandler.row = row;
+    gemHandler.column = column;
+    gem.position.set(column * this._columnWidth, row * this._rowHeight, 0);
+    return gemHandler;
   }
 
   public get selectedGem(): THREE.Object3D | null {
@@ -98,5 +108,62 @@ export default class BoardHandler extends RE.Component {
 
   private onTweenCompleted() {
     this._selectedGem = null;
+  }
+
+  private getGem(column: number, row: number): GemHandler {
+    const gem = this.object3d.children.find(child => {
+      const gemHandler = RE.getComponent(GemHandler, child) as GemHandler;
+      return gemHandler.column === column && gemHandler.row === row;
+    });
+    const gemHandler = RE.getComponent(GemHandler, gem) as GemHandler;
+    return gemHandler;
+  }
+
+  private checkForMatches() {
+    this._matchPause = true;
+    // Check horizontal matches
+    for (let row = 0; row < this._rows; row++) {
+      let rowGems: GemHandler[] = [];
+      for (let col = 0; col < this._columns; col++) {
+        const gem = this.getGem(col, row);
+        rowGems.push(gem);
+      }
+      // Check for matches in the row
+      this.checkForMatchesInRow(rowGems);
+    }
+  }
+
+  private checkForMatchesInRow(rowGems: GemHandler[]) {
+    let currentColor = -1;
+    let matchCount = 1;
+    let matchingGems: GemHandler[] = [];
+
+    for (let gem of rowGems) {
+      if (currentColor === -1) {
+        currentColor = gem.color;
+        matchingGems.push(gem);
+      }
+      else if (gem.color === currentColor) {
+        matchCount++;
+        matchingGems.push(gem);
+      }
+      else {
+        // We have a new color, check if we have a match of 3 or more
+        if (matchCount >= 3) {
+          RE.Debug.log(`Found match of ${matchCount} gems`);
+          for (let gem of matchingGems) {
+            let gemRow = gem.row;
+            let gemColumn = gem.column;
+            gem.object3d.parent?.remove(gem.object3d);
+            this.createRandomGemAt(gemColumn, gemRow);
+          }
+        }
+
+        // Reset for the next potential match
+        currentColor = gem.color;
+        matchCount = 1;
+        matchingGems = [gem];
+      }
+    }
   }
 }
